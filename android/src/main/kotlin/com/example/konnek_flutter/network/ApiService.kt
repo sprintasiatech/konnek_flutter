162bfd75f3c62f49fd073b1eea738ca24da4aa54
Fauzan Akmal Mahdi <fauzan.akmal@sprintasia.co.id>
2025-04-25 10:11:55 +0700

    update: clean kotlin code

--- android/src/main/kotlin/com/example/konnek_flutter/network/ApiService.kt
+++ android/src/main/kotlin/com/example/konnek_flutter/network/ApiService.kt
@@ -17,7 +17,6 @@ interface ApiService {
     ): Response<GetConfigResponseModel>
 
     // GET - Get Chat Conversation
-//    @GET("room/conversation/{roomId}?page={currentPage}&limit={limit}&session_id={sessionId}")
     @GET("room/conversation/{roomId}")
     suspend fun getConversation(
         @Path("roomId") roomId: String,
@@ -32,16 +31,11 @@ interface ApiService {
     suspend fun sendChat(
         @Path("clientId") clientId: String,
         @Body body: RequestBody,
-//        @Body body: Map<*, *>,
-//        @Field("name") name: String,
-//        @Field("text") text: String? = null,
-//        @Field("username") username: String,
     ): Response<SendChatResponseModel>
 
     // POST - Send Chat with Media
     @Multipart
     @POST("chat/media")
-    // @Headers("Content-Type: multipart/form-data")
     suspend fun uploadMedia(
         @Part media: MultipartBody.Part,
         @Part("message_id") messageId: RequestBody,
@@ -51,41 +45,4 @@ interface ApiService {
         @Header("Authorization") token: String,
         @Header("Access-Control-Allow-Origin") accessControlAllowOrigin: String? = "*",
     ): Response<UploadMediaResponseModel>
-
-
-//    // GET - Fetch all items
-//    @GET("posts")
-//    suspend fun getPosts(): Response<List<Post>>
-//
-//    // GET - Fetch single item by ID
-//    @GET("posts/{id}")
-//    suspend fun getPostById(@Path("id") id: Int): Response<Post>
-//
-//    // GET - With query parameters
-//    @GET("posts")
-//    suspend fun getPostsByUserId(
-//        @Query("userId") userId: Int,
-//        @Query("_sort") sort: String = "id",
-//        @Query("_order") order: String = "desc"
-//    ): Response<List<Post>>
-//
-//    // POST - Create new item
-//    @POST("posts")
-//    suspend fun createPost(@Body post: Post): Response<Post>
-//
-//    // PUT - Update existing item
-//    @PUT("posts/{id}")
-//    suspend fun updatePost(
-//        @Path("id") id: Int,
-//        @Body post: Post
-//    ): Response<Post>
-//
-//    // DELETE - Remove item
-//    @DELETE("posts/{id}")
-//    suspend fun deletePost(@Path("id") id: Int): Response<Unit>
-//
-//    // Example with custom headers
-//    @Headers("Content-Type: application/json", "Accept: application/json")
-//    @POST("auth/login")
-//    suspend fun login(@Body credentials: Map<String, String>): Response<ApiResponse<String>>
 }
